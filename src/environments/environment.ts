315f8aa0790a3e6e39dba8696918fc9d9523bc36
Matt <mateo19dg@gmail.com>
2025-05-28 14:11:52 -0500

    initial config

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://fqupvbwqjfacmlmtkaqi.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxdXB2YndxamZhY21sbXRrYXFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NDM2MDUsImV4cCI6MjA2NDAxOTYwNX0.hyGOuRi1K8cAAQ5PxjJZg6ADYQdArIGd8V9P710p2Co',
+}
